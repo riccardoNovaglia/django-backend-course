f5fd5aae1e4124728d17c6f2e8d237f54cc65e9f
Riccardo Novaglia <rnovaglia@thoughtworks.com>
2021-12-09 17:24:11 +0000

    Add API to generate API tokens for users

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -56,3 +57,33 @@ class PublicUserApiTest(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_token_created_for_user(self):
+        payload = {'email': 'some@email.com', 'password': 'irrelevant'}
+        create_user(**payload)
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_return_400_when_creds_are_invalid(self):
+        create_user(email='test@test.com', password='good pass')
+        payload = {'email': 'test@test.com', 'password': 'bad pass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_return_400_if_user_not_found(self):
+        payload = {'email': "test@test.com", 'password': 'irrelevant'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_returns_400_if_password_not_in_req(self):
+        res = self.client.post(TOKEN_URL, {'email': 'test@test.com'})
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
